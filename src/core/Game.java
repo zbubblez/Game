93cd2a153238cc9ceced440aa320bd161c3e094f
Dan <zbubblez@hotmail.com>
2015-11-19 15:10:20 -0700

    Penis

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,11 +1,33 @@
 package core;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+import ui.BufferPanel;
 import utilities.Tools;
 
 public class Game {
-	
+
+	private BufferPanel panel = null;
+	private JFrame gameFrame = new JFrame(Constants.name + "   -- " + Constants.version);
+
 	public Game() {
-		Tools.log("Fuck you marrk");
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setSize(500, 500);
+		panel = new BufferPanel(this);
+		gameFrame.add(panel, BorderLayout.CENTER);
+		gameFrame.setVisible(true);
 	}
-	
+
+	public void paint(Graphics g) {
+		Tools.drawSharpText("Penis", 25, 25, Color.BLUE, Color.BLACK, g);
+	}
+
+	public static void main(String[] arg0) {
+		new Game();
+	}
+
 }
--- src/ui/BufferPanel.java
+++ src/ui/BufferPanel.java
@@ -1,8 +1,11 @@
 package ui;
 
+import java.awt.Image;
+
 import javax.swing.JPanel;
 
 import core.Game;
+import utilities.Tools;
 
 public class BufferPanel extends JPanel implements Runnable {
 
@@ -12,14 +15,23 @@ public class BufferPanel extends JPanel implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	private Game game = null;
+	private Image buffer = null;
 
 	public BufferPanel(Game game) {
 		this.game = game;
+		new Thread(this).start();
 	}
 
 	@Override
 	public void run() {
-		
+		while (true) {
+			buffer = this.createImage(this.getWidth(), this.getHeight());
+			if (buffer != null && buffer.getGraphics() != null) {
+				game.paint(buffer.getGraphics());
+				this.getGraphics().drawImage(buffer, 0, 0, null);
+			}
+			Tools.sleep(5);
+		}
 	}
 
 }
